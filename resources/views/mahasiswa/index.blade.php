211dab2fd2d45f97b344eda0a0d3faf1e7de4e79
idham <idhamkholid965@gmail.com>
2019-08-06 13:35:02 +0700

    add database

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -6,6 +6,34 @@
     <div class="row">
         <div class="col-lg-10 mt-3">
             <h1>Daftar Mahasiswa</h1>
+
+            <table class="table">
+                <thead class="thead-dark">             
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">NIM</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Jurusan</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    @foreach ($mahasiswa as $mhs)
+                        <td scope="row">{{$loop->iteration}}</td>
+                        <td>{{$mhs->nama}}</td>
+                        <td>{{$mhs->NIM}}</td>
+                        <td>{{$mhs->email}}</td>
+                        <td>{{$mhs->jurusan}}</td>
+                        <td>
+                            <a href="" class="badge badge-success">edit</a>
+                            <a href="" class="badge badge-danger">delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
